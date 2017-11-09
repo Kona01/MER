9dcfa089974199da935aa8c3d2b7b32f929d4725
Kona01 <matthewjacobs@hotmail.ca>
2017-11-09 09:47:52 -0800

    Added Perogie and Deleted All Non-Functioning Dimension Features

--- src/main/java/mer/mod/items/ItemPerogie.java
+++ src/main/java/mer/mod/items/ItemPerogie.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemFood;
 public class ItemPerogie extends ItemFood {
 
     public ItemPerogie() {
-        super(8, 0.2F, false);
+        super(8, 0.2F, true);
         setUnlocalizedName(Reference.ModItems.PEROGIE.getUnlocalizedName());
         setRegistryName(Reference.ModItems.PEROGIE.getRegistryName());
         setCreativeTab(MER.CREATIVE_TAB);
